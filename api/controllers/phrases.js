b3db2ca093f75431f625c581b453ff795808516a
Carlos Nicolas Alarcon <c.alarcon@globant.com>
2019-11-03 19:34:35 -0300

    Add get and post request phrases

--- api/controllers/phrases.js
+++ api/controllers/phrases.js
@@ -6,12 +6,36 @@ const getPhrases = (req, res) => {
         .catch( err => { throw err })
 }
 
+const getPhrasesByPatient = async (req, res) => {
+    const folder = await getById('folders', req.params.id, 'patient_id', 'id');
+    const phrasesInFolder = await getById('phrases_folder', folder[0].id, 'folder_id', 'phrase_id');
+    const phrases = [];
+    for (const phraseId of phrasesInFolder) {
+        const phrase = await getById('phrases', phraseId.phrase_id);
+        phrases.push(phrase[0]);
+    }
+    const sendPhrases = [];
+    for( const phrase of phrases) {
+        const pictogramsInPhrase = await getById('pictograms_phrase', phrase.id, 'phrase_id');
+        const pictograms = [];
+        for( const pictogramInPhrase of pictogramsInPhrase) {
+            console.log('pictogramInPhrase', pictogramInPhrase)
+            const pictogram = await getById('pictograms', pictogramInPhrase.pictogram_id);
+            pictograms.push(pictogram[0])
+        }
+        phrase['pictograms'] = pictograms;
+        sendPhrases.push(phrase);
+    }
+    res.status(200).json(sendPhrases)
+}
+
 const createPhrases = async (req, res) => {
+    console.log('body', req.body)
     const folder = await getById('folders', req.params.id, 'patient_id', 'id');
     const idFolder = folder[0].id;
     const phrase = await insert('phrases', { type: req.body.type })
     const idPhrase = phrase.id;
-    await insert('phrases-folder', { folder_id: idFolder, phrase_id: idPhrase });
+    await insert('phrases_folder', { folder_id: idFolder, phrase_id: idPhrase });
     let pictograms = req.body.pictograms;
     console.log('add pictograms: ', pictograms)
     let newPictograms = [];
@@ -35,12 +59,15 @@ const updatePhrases = (req, res) => {
 }
 
 const deletePhrases = (req, res) => {
-    
+    del('phrases', req.params)
+        .then( response => response ? res.status(200).json({ 'delete': 'success'}) : res.status(500).json({ err: 'error'}))
+        .catch( err => { throw err });
 }
 
 module.exports = {
     getPhrases,
     createPhrases,
     updatePhrases,
-    deletePhrases    
+    deletePhrases,
+    getPhrasesByPatient    
 }
--- api/routes/phrases.js
+++ api/routes/phrases.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const phrases = require('../controllers/phrases');
 
 router.get('/', phrases.getPhrases);
+router.get('/:id', phrases.getPhrasesByPatient);
 router.post('/:id', phrases.createPhrases);
 router.put('/:id', phrases.updatePhrases);
 router.delete('/:id', phrases.deletePhrases);
