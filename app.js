eb9e8bda46db144809b46add09a45e825ea9c8e0
Nicolas <c.alarcon@globant.com>
2020-05-14 23:55:19 -0300

    Change message for heroku

--- app.js
+++ app.js
@@ -29,7 +29,7 @@ app.use((req, res, next)=> {
 });
 
 app.get('/', function(req, res){
-    res.status(200).send({Conectado: 'API corriendo'});
+    res.status(200).send({Conectado: 'API corriendo desde heroku!'});
 });
 
 app.use('/categories', categoriesRouter);
